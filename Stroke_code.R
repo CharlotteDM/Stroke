edd1e98f0b48d204d4e50fdee4713aec209b5912
CharlotteDM <carolinemularczyk@gmail.com>
2024-05-24 14:02:17 +0200

    adding data - stroke.csv

--- Stroke_code.R
+++ Stroke_code.R
@@ -1,1 +1,20 @@
 #Project "Stroke Prediction"
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+library("gganimate")
+library("ggrepel")
+library("rworldmap")
+library("RColorBrewer")
+library("knitr")
+library("plotly")
+library("htmlwidgets")
+library("GGally")
+library("stats")
+library("rstudioapi")
+
+path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(path)
+
+stroke <- read.csv("data/healthcare-dataset-stroke-data.csv", 
+                   stringsAsFactors = F)
