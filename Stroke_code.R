3fcf6926ebf23652d9e7e27d58f7aec8cade7e6c
CharlotteDM <carolinemularczyk@gmail.com>
2024-05-24 20:38:59 +0200

    adding simple plots

--- Stroke_code.R
+++ Stroke_code.R
@@ -28,3 +28,28 @@ names(stroke) #column names
 summary(stroke) #summary for each columns
 skim(stroke) #missing values, quantile, etc.
 create_report(stroke) #full data profile with visualizations
+
+
+#plot: gender
+gender <- ggplot(stroke, aes(x=reorder(gender, gender, function(x)-length(x)))) +
+  geom_bar(fill='lightblue') +  labs(x='Gender')
+gender
+
+
+#plot: type of residence
+residence <- ggplot(stroke, aes(x=reorder(Residence_type, Residence_type, function(x)-length(x)))) +
+  geom_bar(fill='lightgreen') +  labs(x='Type of Residence')
+residence
+
+#plot: smoking status
+smoking <- ggplot(stroke, aes(x=reorder(smoking_status, smoking_status, function(x)-length(x)))) +
+  geom_bar(fill='lightpink') +  labs(x='Smoking Status')
+smoking
+
+#correlation
+cor(stroke$age, stroke$hypertension, use = "complete.obs")
+
+
+
+
+
